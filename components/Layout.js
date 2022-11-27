d7a735777b53416af2ec57701a6c65d9d1e0e225
takumi <takumim1005@gmail.com>
2022-11-27 21:42:50 +0900

    💄 utils作成と適用

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
 import styles from "./layout.module.css";
 
 const name = "Shin Code";
@@ -11,8 +12,11 @@ function Layout({ children }) {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<header className={styles.header}>
-				<img src="/images/profile.png" />
-				<h1>{name}</h1>
+				<img
+					src="/images/profile.png"
+					className={utilStyles.borderCircle}
+				/>
+				<h1 className={utilStyles.heading2Xl}>{name}</h1>
 			</header>
 			<main>{children}</main>
 		</div>
