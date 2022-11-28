f0e551dae18d3653802cdbe3474d6d0a0533ce10
takumi <takumim1005@gmail.com>
2022-11-28 22:45:39 +0900

    SSGを実装

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import { getPostsData } from "../lib/posts";
 import styles from "../styles/Home.module.css";
 import utilStyle from "../styles/utils.module.css";
 
-export default function Home() {
+// SSGの場合
+export async function getStaticProps() {
+	const allPostsData = getPostsData(); // id, title, date, thumbnail
+	console.log(allPostsData);
+
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+}
+
+export default function Home({ allPostsData }) {
 	return (
 		<Layout>
 			<section className={utilStyle.headingMd}>
