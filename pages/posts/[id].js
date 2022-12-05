452f9eb9c8a83c0a74abd1684f1dd514cff5efe2
takumi <takumim1005@gmail.com>
2022-12-05 21:43:10 +0900

    SSGの実装

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Layout from "../../components/Layout";
-import { getAllPostIds } from "../../lib/post";
+import { getAllPostIds, getPostData } from "../../lib/post";
 
 export async function getStaticPaths() {
 	const paths = getAllPostIds();
@@ -10,6 +10,25 @@ export async function getStaticPaths() {
 	};
 }
 
-export default function Post() {
-	return <Layout>動的ルーティング</Layout>;
+export async function getStaticProps({ params }) {
+	const postData = await getPostData(params.id);
+
+	return {
+		props: {
+			postData,
+		},
+	};
+}
+
+export default function Post({ postData }) {
+	return (
+		<Layout>
+			{postData.title}
+			<br />
+			{postData.date}
+			<br />
+			{postData.blogContentHTML}
+			<br />
+		</Layout>
+	);
 }
